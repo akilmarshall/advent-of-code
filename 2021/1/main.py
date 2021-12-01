6bf2fca181a69458c1f7c5d117340dd23565aa32
akil marshall <akilmarshall@gmail.com>
2021-11-30 22:40:12 -0800

    reduced sloc in

--- 2021/1/main.py
+++ 2021/1/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def inputs(fname):
     with open(fname) as f:
         for line in f.readlines():
@@ -8,31 +11,31 @@ class Day1:
     def __init__(self, fname='input'):
         self.fname = fname
 
-    def part1(self):
+    def f(self, values: List[int]):
+        '''
+        given a sequence count the number of times it incremented
+        '''
         inc = 0
         prev = None
-        for x in inputs(self.fname):
+        for x in values:
             cur = int(x)
             if prev is not None:
                 if prev < cur:
                     inc += 1
             prev = cur
-        print(inc)
+        return inc
+
+    def part1(self):
+        values = [int(x) for x in inputs(self.fname)]
+        print(self.f(values))
 
     def part2(self):
         def windows():
+            # computes a list of 3 value sums of the input
             out = []
             l = list(inputs(self.fname))
             for a, b, c in zip(l[0:], l[1:], l[2:]):
                 out.append(int(a) + int(b) + int(c))
             return out
 
-        inc = 0
-        prev = None
-        for x in windows():
-            cur = int(x)
-            if prev is not None:
-                if prev < cur:
-                    inc += 1
-            prev = cur
-        print(inc)
+        print(self.f(windows()))
