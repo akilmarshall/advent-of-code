a3a7b8a5004cb66cfe394d0ac4444feacc60168f
Akil Marshall <akilmarshall@gmail.com>
2022-12-05 19:46:12 -1000

    unified part 1 & 2

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -5,19 +5,23 @@ with open("input") as f:
         buffer = line.strip()
 
 def part_1():
-    i = 4
-    for X in zip(buffer, buffer[1:], buffer[2:], buffer[3:]):
-        if len(set(X)) == 4:
-            break
-        i += 1
-    print(i)
-
-def part_2():
+    magic = 4
     i = 0
     while True:
-        j = i + 14
+        j = i + magic
         seg = set(buffer[i:j])
-        if len(seg) == 14:
+        if len(seg) == magic:
+            break
+        i += 1
+    print(j)
+
+def part_2():
+    magic = 14
+    i = 0
+    while True:
+        j = i + magic
+        seg = set(buffer[i:j])
+        if len(seg) == magic:
             break
         i += 1
     print(j)
